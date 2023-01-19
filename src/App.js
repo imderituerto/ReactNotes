f9c9f5cc6882f29fabc014722b2ea67e2ab1ac2f
Isabel <isamdrituerto@hotmail.com>
2023-01-19 12:42:23 +0100

    Added counter to App.js file using Javascript

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 function App() {
   
   // javascript goes here
+  let counter = 0;
+
+  const incrementCounter = () => {
+    counter += 1;
+  }
   
   return (
-    <div class="blue">
+    <div className="blue">
     <h1>Hello world!</h1>
     <p>Welcome to my React app.</p>
     </div>
