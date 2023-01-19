9d522a40d9e6e1e1eb21c831ba34876623590660
Isabel <isamdrituerto@hotmail.com>
2023-01-19 12:38:11 +0100

    Added styling to App.css and implemented with div tags in App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   // javascript goes here
   
   return (
-    <>
+    <div class="blue">
     <h1>Hello world!</h1>
     <p>Welcome to my React app.</p>
-    </>
+    </div>
   );
 }
 
